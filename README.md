6592cbbd2dd82da388e1dc5e0ba2b96424d33ee2
Saahil <suberoi@dux.com.au>
2022-06-20 10:10:07 +1000

    Updated Serial Number Upload - New UI - Improved Performance

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # README #
-
-This README would normally document whatever steps are necessary to get your application up and running.
+The application inputs CSV files from the user and sends the serial numbers to M3 via API: SOS100MI/AddIndItem/
 
 ### What is this repository for? ###
+The application inputs CSV files from the user, wrangles the data and sends the serial numbers over to Movex.
+* To send data to via M3 API: SOS100MI/AddIndItem/. 
+* The item description is retrieved from m3 via API: MMS200MI/GetItmBasic/
+* There is a config.py file with all the configurations used in the application.
 
-* Quick summary
-* Version
-* [Learn Markdown](https://bitbucket.org/tutorials/markdowndemo)
+### Backend Framework ###
+The application is built using Flask Framework and deployed using gUnicorn. A gUnicorn_config.py file holds all the deployment configurations and commands.
+
+### Frontend Framework ###
+The front-end is developed using HTML/CSS and Vanilla JS. 
+* A template folder is created with all the required HTML files.
+* Static folder is there with all the css files. For styling materialize css has been used.
+* All the JS is stored under the folder scripts.
+
+### Version ###	
+* Version V1.0: For Continuous Gas Flow
 
 ### How do I get set up? ###
-
 * sudo apt-get upgrade -y
 * Create a virtual environment. python3 -m venv <**name_of_virtualEnv**>
 * Start the virtual environment. source **name_ov_virtualEnv**/bin/activate
@@ -17,15 +27,12 @@ This README would normally document whatever steps are necessary to get your app
 * pip install -r requirement.txt
 * export FLASK_APP=app.py
 * export FLASK_ENV=development/production (based on env)
-* 
+* flask run
 
 ### Contribution guidelines ###
-
-* Writing tests
-* Code review
-* Other guidelines
+* For checking all the connections, go to test.py and check all the connections
+* Code review: Author
 
 ### Who do I talk to? ###
-
-* Repo owner or admin
-* Other community or team contact
+* Repo owner: Dux Manufacturing Limited
+* Author: Saahil Uberoi 
--- m3.py
+++ m3.py
@@ -2,35 +2,25 @@ import json
 import requests
 import config as cfg
 
-"""
-Get Item Description Information from M3
-"""
-
-
-def get_ITDS_from_M3(params):
-    response = requests.get(cfg.url + API_call.itds_api + API_call.itds_transaction, params=params, headers=cfg.headers, auth=cfg.auth)
-    _ITDS_list = json.loads(response.text)
-    for key in _ITDS_list['MIRecord']:
-        for key_2 in key['NameValue']:
-            if key_2['Name'] == 'ITDS':
-                ITDS = key_2['Value']
-                break
-        return ITDS
-
-
 """ 
 This route will make the API calls to M3
 """
 
 
-class API_call:
+class API_call():
+
+    # Receive Item Description from M3
     itds_api = 'MMS200MI/'
     itds_transaction = 'GetItmBasic/'
+
     # Send Data to M3
     send_api = 'SOS100MI/'
     send_transaction = 'AddIndItem/'
 
-    # Form data to send to M3
+    """
+    Function to set data in the correct format for M3
+    """
+
     def data_to_m3(ITDS, val):
         _full_date = '20' + str(val['MANUFACTURING DATE'])
         _year = '20' + str(val['MANUFACTURING DATE'])[0:2]
@@ -57,11 +47,30 @@ class API_call:
         return data_list
 
 
-# Send to M3
+"""
+Get Item Description Information from M3
+"""
+
+
+def get_ITDS_from_M3(params):
+    response = requests.get(cfg.url + API_call.itds_api + API_call.itds_transaction, params=params, headers=cfg.headers,
+                            auth=cfg.auth)
+    _ITDS_list = json.loads(response.text)
+    for key in _ITDS_list['MIRecord']:
+        for key_2 in key['NameValue']:
+            if key_2['Name'] == 'ITDS':
+                ITDS = key_2['Value']
+                break
+        return ITDS
+
+
+"""
+This route will send data to M3
+"""
+
+
 def send_data(data_list):
     try:
-        # print('Sending data to M3')
-        # print(data_list)
         response = requests.get(cfg.url + API_call.send_api + API_call.send_transaction, params=data_list,
                                 headers=cfg.headers,
                                 auth=('INFORBC\#DUXFEA', 'L3t5F1x$TufF12345'))
